672b9512d3ffefa82ea5bb1ff6a56b6190e9b242
burlacugeorge <burlacugeorge22@gmail.com>
2025-11-17 18:18:59 +0200

    Am adaugat un nou branch "Tema11NoiembrieSelenium" cu tema Demo QA Test

--- src/test/java/DemoQATest.java
+++ src/test/java/DemoQATest.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
